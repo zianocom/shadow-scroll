8e18a9867073d80d7c9b19c21ec988509b4ab13d
cute.com._. (lovely.com._.) <jolee0816@gmail.com>
2026-02-17 16:45:12 +0000

    shadow-scroll.pages.dev 페이지를 찾을 수 없음 다음 웹 주소(https://shadow-scroll.pages

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ const nextConfig: NextConfig = {
   output: 'export',
   /* config options here */
   trailingSlash: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
