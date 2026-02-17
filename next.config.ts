1db6c9cd92c730ea11cc363f5f867e23341e3778
cute.com._. (lovely.com._.) <jolee0816@gmail.com>
2026-02-17 16:41:43 +0000

    계속 같은 에러야

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   trailingSlash: true,
   typescript: {
@@ -10,6 +11,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
